9ecb3113d0740c888620d513cdbd086e12e5e3d9
Bhupati Parmar <87934381+Kennys13@users.noreply.github.com>
2023-08-01 08:05:04 +0530

    Update bipul.sol

--- contracts/bipul.sol
+++ contracts/bipul.sol
@@ -6,7 +6,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenGamingToken is ERC20, Ownable {
 
-    constructor() ERC20("Degen Gaming Token", "DGT") {}
+    struct Item {
+        uint256 itemId;
+        string name;
+        uint256 price;
+        uint256 quantity;
+    }
+
+    Item[] public items;
+
+    constructor() ERC20("Degen Gaming Token", "DGT") {
+        // Initialize some sample items. You can modify or add more items as needed.
+        items.push(Item(0, "PEN", 100, 100));
+        items.push(Item(1, "PENCIL", 200, 50));
+    }
 
     // Mint new tokens and distribute them to players as rewards
     function mint(address to, uint256 amount) public onlyOwner {
@@ -19,10 +32,17 @@ contract DegenGamingToken is ERC20, Ownable {
     }
 
     // Players can redeem their tokens for items in the in-game store
-    function redeem(uint256 amount) public {
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-        _burn(msg.sender, amount);
-        // Add your redemption logic here for in-game items or other rewards
+    function redeem(uint256 itemId, uint256 quantity) public {
+        require(itemId < items.length, "Invalid item ID");
+        uint256 totalCost = items[itemId].price * quantity;
+        require(balanceOf(msg.sender) >= totalCost, "Insufficient balance");
+        require(items[itemId].quantity >= quantity, "Insufficient quantity of item");
+
+        // Deduct the redeemed tokens from the player's account
+        _transfer(msg.sender, address(this), totalCost);
+
+        // Reduce the quantity of the redeemed item
+        items[itemId].quantity -= quantity;
     }
 
     // Anyone can burn tokens they own if they are no longer needed
@@ -30,4 +50,7 @@ contract DegenGamingToken is ERC20, Ownable {
         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
         _burn(msg.sender, amount);
     }
+
+    // Event to indicate the redeemed amount
+    event RedemptionAmount(address indexed player, uint256 amount);
 }
